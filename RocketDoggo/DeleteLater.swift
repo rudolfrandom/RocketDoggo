7b2ed17dcc80034ad5739950f55e0599d76066e1
Martine Onstad <martine.onstad1@gmail.com>
2018-01-06 22:30:44 +0100

    Lagt til klasse for research av drag and drop

---
+++ RocketDoggo/DeleteLater.swift
@@ -0,0 +1,12 @@
+//
+//  DeleteLater.swift
+//  RocketDoggo
+//
+//  Created by Martine Onstad on 06/01/2018.
+//  Copyright © 2018 Ole Onstad. All rights reserved.
+//
+
+
+class DeleteLater: SKScene {
+
+}
