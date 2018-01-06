7180a4773db7677192893002581e4b73a85e1a65
Martine Onstad <martine.onstad1@gmail.com>
2018-01-06 22:03:12 +0100

    Styingsmekanisme endret

--- RocketDoggo/Player.swift
+++ RocketDoggo/Player.swift
@@ -15,23 +15,28 @@ class Player: SKScene, SKPhysicsContactDelegate {
     private var topSpeed: CGFloat = 1.0
     private var verticalSpeed: CGFloat = 0.0
     private var verticalPosition: CGFloat = 0.0
+    private var xPositionToMoveTo: CGFloat = 0.0
 
     private let topPart = TopPart()
     private let bodyPart = BodyPart()
     private let boosterPart = BoosterPart()
 
+    var stimer = Timer()
+    
     public var top = SKShapeNode(), body = SKShapeNode(), booster = SKShapeNode()
     
     private var timer = Timer()
 
     /// Spawns the rocket
-    func spawn() -> Void
+    func spawn(toScene scene: SKScene) -> Void
     {
-        setupRocket()
+        setupRocket(toScene: scene)
+        
+        stimer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(self.xxx), userInfo: nil, repeats: true)
     }
     
     /// Connects all the rocket parts
-    private func setupRocket() -> Void
+    private func setupRocket(toScene scene: SKScene) -> Void
     {
         /* Sets up the top part */
         top = SKShapeNode(rectOf: CGSize(width: self.topPart.width, height: self.topPart.height))
@@ -61,7 +66,7 @@ class Player: SKScene, SKPhysicsContactDelegate {
         self.addChild(top)
         top.addChild(body)
         body.addChild(booster)
-
+        scene.addChild(self)
     }
     
     /// Returns the height of the rocket
@@ -73,21 +78,20 @@ class Player: SKScene, SKPhysicsContactDelegate {
     {
         return self.boosterPart.height
     }
-    
+
     func addAcceleration() -> Void
     {
         timer.invalidate()
         timer = Timer.scheduledTimer(timeInterval: self.acceleration,
                                      target: self, selector: #selector(self.speedUp), userInfo: nil, repeats: true)
     }
-    
+
     func removeAcceleration() -> Void
     {
         timer.invalidate()
         timer = Timer.scheduledTimer(timeInterval: self.acceleration, target: self, selector: #selector(self.speedDown), userInfo: nil, repeats: true)
-
     }
-    
+
     @objc func speedUp()
     {
         if self.verticalSpeed <= self.topSpeed
@@ -102,16 +106,41 @@ class Player: SKScene, SKPhysicsContactDelegate {
             self.verticalSpeed -= 0.1
         }
     }
-    func updateVerticalPosition() -> Void
-    {
-        self.verticalPosition += self.verticalSpeed / 100
-    }
+
     func updateVerticalPosition() -> CGFloat
     {
         self.verticalPosition += self.verticalSpeed / 100
         return self.verticalPosition
     }
-
+    
+    func moveHorizontally(to xPosition: CGPoint) -> Void
+    {
+        self.xPositionToMoveTo = xPosition.x
+        //print(xPositionToMoveTo)
+        //self.top.position = yPosision
+        
+        //stimer = Timer.scheduledTimer(timeInterval: 0.6, target: self, selector: #selector(self.xxx), userInfo: nil, repeats: true)
+    }
+    var first = true
+    func update() -> Void
+    {
+        if (first == true)
+        {
+            first = false
+            
+            //stimer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(self.xxx), userInfo: nil, repeats: true)
+        }
+    }
+    @objc func xxx() {
+        
+        if xPositionToMoveTo > self.top.position.x + 2 {
+            self.top.position.x += 3
+            
+        }
+        else if xPositionToMoveTo < self.top.position.x - 2 {
+            self.top.position.x -= 3
+        }
+    }
 }
 
 class TopPart {
